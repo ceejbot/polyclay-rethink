10caca1136911c4a5ea2532a108b238e4e848f7d
C J Silverio <ceej@npmjs.com>
2015-02-14 23:54:15 -0800

    Almost but not quite a working rethinkdb adapter.
    The `.all()` test is failing for reasons I do not understand.
    It fetches all the data correctly, then fails in the driver
    internals some time after I call back with the data.

---
+++ index.js
@@ -0,0 +1,324 @@
+var
+    _       = require('lodash'),
+    async   = require('async'),
+    events  = require('events'),
+    Rethink = require('rethinkdb'),
+    util    = require('util')
+    ;
+
+var RethinkAdapter = module.exports = function RethinkAdapter()
+{
+    events.EventEmitter.call(this);
+};
+util.inherits(RethinkAdapter, events.EventEmitter);
+
+RethinkAdapter.prototype.connection  = null;
+RethinkAdapter.prototype.dbname      = null;
+RethinkAdapter.prototype.constructor = null;
+RethinkAdapter.prototype.options     = null;
+RethinkAdapter.prototype.attempts    = 0;
+
+RethinkAdapter.prototype.configure = function(opts, modelfunc)
+{
+    this.options = opts;
+    this.tablename = opts.dbname || modelfunc.prototype.plural;
+    this.constructor = modelfunc;
+
+    this.connect();
+};
+
+RethinkAdapter.prototype.connect = function()
+{
+    var self = this;
+
+    var opts =
+    {
+        host:    this.options.host || 'localhost',
+        port:    this.options.port || 28015,
+        authKey: this.options.authKey,
+        db:      this.options.database,
+    };
+
+    Rethink.connect(opts)
+    .then(function(conn)
+    {
+        self.connection = conn;
+        self.emit('ready');
+    }).done();
+};
+
+function exponentialBackoff(attempt)
+{
+    return Math.min(Math.floor(Math.random() * Math.pow(2, attempt) + 10), 10000);
+}
+
+RethinkAdapter.prototype._createTable = function _createTable(tname, callback)
+{
+    var self = this;
+
+    self.db.tableList()
+    .run(self.connection, function(err, tables)
+    {
+        if (err) return callback(err);
+        if (tables.indexOf(tname) > -1)
+            return callback();
+
+        var tableOpts = self.options.dbopts || {};
+        tableOpts.primaryKey = self.constructor.prototype.keyfield;
+        self.db.tableCreate(tname, tableOpts).run(self.connection, callback);
+    });
+};
+
+RethinkAdapter.prototype.provision = function(callback)
+{
+    var self = this;
+    self.db = Rethink.db(self.options.database);
+    self.objects = Rethink.table(self.tablename);
+    self.attachments = Rethink.table(self.tablename + '_attachments');
+
+    // TODO cleanup-- yuck
+    function makeAttachments()
+    {
+        self._createTable(self.tablename + '_attachments', callback);
+    }
+
+    Rethink.dbList().run(self.connection, function(err, dbs)
+    {
+        if (err) return callback(err);
+        if (dbs.indexOf(self.options.database) > -1)
+            return self._createTable(self.tablename, makeAttachments);
+        Rethink.dbCreate(self.options.database)
+        .run(self.connection, function(err, ignored)
+        {
+            if (err) return callback(err);
+            self._createTable(self.tablename, makeAttachments);
+        });
+    });
+};
+
+RethinkAdapter.prototype.all = function all(callback)
+{
+    var self = this;
+
+    self.objects
+        .orderBy(self.constructor.prototype.keyfield)
+        .run(self.connection, function(err, cursor)
+    {
+        if (err) return callback(err);
+
+        cursor.toArray(function(err, results)
+        {
+            if (err) return callback(err);
+            if (!results || !results.length) return callback(null, []);
+
+            var batched = _.map(results, function(json)
+            {
+                var obj = new self.constructor();
+                obj.initFromStorage(json);
+                return obj;
+            });
+
+            cursor.close();
+            callback(null, batched);
+        });
+    });
+};
+
+RethinkAdapter.prototype.save = function save(object, json, callback)
+{
+    if (!object.key || !object.key.length)
+        throw(new Error('cannot save a document without a key'));
+
+    var self = this;
+    var payload = RethinkAdapter.flatten(json);
+
+    self.objects.insert(payload.body).run(self.connection, function(err, res)
+    {
+        if (err) return callback(err);
+        if (!payload.attachments.length)
+            callback(null, 'OK');
+
+        var actions = _.map(payload.attachments, function(v)
+        {
+            return function(cb) { self.saveAttachment(object, v, cb); };
+        });
+
+        async.parallel(actions, function(err, res)
+        {
+            callback(err, 'OK');
+        });
+    });
+};
+
+RethinkAdapter.prototype.update = function(object, json, callback)
+{
+    var self = this;
+    var payload = RethinkAdapter.flatten(json);
+
+    self.objects
+        .get(object.key)
+        .update(payload.body)
+        .run(self.connection, function(err, res)
+    {
+        if (err) return callback(err);
+        if (!payload.attachments.length)
+            callback(null, 'OK');
+
+        var actions = _.map(payload.attachments, function(v)
+        {
+            return function(cb) { self.saveAttachment(object, v, cb); };
+        });
+
+        async.parallel(actions, function(err, res)
+        {
+            callback(err, 'OK');
+        });
+    });
+};
+
+RethinkAdapter.prototype.get = function(key, callback)
+{
+    var self = this;
+    if (Array.isArray(key))
+        return this.getBatch(key, callback);
+
+    self.objects.get(key).run(self.connection, function(err, json)
+    {
+        if (err) return callback(err);
+        if (!json) return callback();
+
+        var object = new self.constructor();
+        object.initFromStorage(json);
+        callback(null, object);
+    });
+};
+
+RethinkAdapter.prototype.getBatch = function(keylist, callback)
+{
+    var self = this;
+
+    self.objects
+        .getAll(Rethink.args(keylist))
+        .coerceTo('array')
+        .run(self.connection, function(err, results)
+    {
+        if (err) return callback(err);
+        if (!results || !results.length) return callback(null, []);
+
+        var batched = _.map(results, function(json)
+        {
+            var obj = new self.constructor();
+            obj.initFromStorage(json);
+            return obj;
+        });
+
+        callback(null, batched);
+    });
+};
+
+RethinkAdapter.prototype.merge = function(key, attributes, callback)
+{
+    var self = this;
+    self.objects.get(key).update(attributes).run(self.connection, callback);
+};
+
+RethinkAdapter.prototype.remove = function(object, callback)
+{
+    if (!object.key || !object.key.length)
+        throw(new Error('cannot delete a document without a key'));
+
+    var self = this;
+    self.objects.get(object.key).delete().run(self.connection, callback);
+};
+
+RethinkAdapter.prototype.destroyMany = function(objects, callback)
+{
+    var self = this;
+    var ids = _.map(objects, function(obj)
+    {
+        if (typeof obj === 'string')
+            return obj;
+        return obj.key;
+    });
+
+    self.objects
+        .getAll(Rethink.args(ids))
+        .delete({returnChanges: false})
+        .run(self.connection, callback);
+};
+
+var makeAttachmentKey = RethinkAdapter.makeAttachmentKey = function makeAttachmentKey(key, name)
+{
+    return key + ':' + name;
+};
+
+RethinkAdapter.prototype.attachment = function(key, name, callback)
+{
+    var self = this;
+    var akey = makeAttachmentKey(key, name);
+    self.attachments.get(akey).run(self.connection, function(err, attach)
+    {
+        if (err) return callback(err);
+        callback(null, attach.body);
+    });
+};
+
+RethinkAdapter.prototype.saveAttachment = function(object, attachment, callback)
+{
+    var self = this;
+    var akey = makeAttachmentKey(object.key || object, attachment.name);
+
+    // TODO this seems horrible. is there a better way?
+    self.attachments.get(akey).run(self.connection, function(err, existing)
+    {
+        if (err) return callback(err);
+        if (!existing)
+        {
+            attachment[self.constructor.prototype.keyfield] = akey;
+            self.attachments.insert(attachment).run(self.connection, callback);
+        }
+        else
+        {
+            self.attachments.get(akey).update(attachment).run(self.connection, function(err, res)
+            {
+                callback(err);
+            });
+        }
+    });
+};
+
+RethinkAdapter.prototype.removeAttachment = function(object, name, callback)
+{
+    var self = this;
+    var akey = makeAttachmentKey(object.key, name);
+    self.objects.get(akey).delete().run(self.connection, callback);
+};
+
+RethinkAdapter.prototype.shutdown = function(callback)
+{
+    if (!this.connection) return callback();
+    this.connection.close(callback);
+};
+
+RethinkAdapter.flatten = function(json)
+{
+    var payload = {};
+    payload.attachments = [];
+
+    if (json._attachments)
+    {
+        var attaches = Object.keys(json._attachments);
+        for (var i = 0; i < attaches.length; i++)
+        {
+            var attachment = json._attachments[attaches[i]];
+            payload.attachments.push({
+                name: attaches[i],
+                body: attachment.body
+            });
+        }
+        delete json._attachments;
+    }
+
+    payload.body = _.clone(json);
+    return payload;
+};
---
+++ test/test-rethink.js
@@ -0,0 +1,492 @@
+/*global describe:true, it:true, before:true, after:true */
+
+var
+	demand   = require('must'),
+	events   = require('events'),
+	fs       = require('fs'),
+	path     = require('path'),
+	polyclay = require('polyclay'),
+	Rethink  = require('rethinkdb'),
+	Adapter  = require('../index')
+	;
+
+var testDir = process.cwd();
+if (path.basename(testDir) !== 'test')
+	testDir = path.join(testDir, 'test');
+var attachmentdata = fs.readFileSync(path.join(testDir, 'test.png'));
+
+describe('rethinkdb adapter', function()
+{
+	var modelDefinition =
+	{
+		properties:
+		{
+			key:           'string',
+			name:          'string',
+			created:       'date',
+			foozles:       'array',
+			snozzers:      'hash',
+			is_valid:      'boolean',
+			count:         'number',
+			required_prop: 'string',
+			ttl:           'number'
+		},
+		optional: [ 'computed', 'ephemeral' ],
+		required: [ 'name', 'is_valid', 'required_prop'],
+		singular: 'model',
+		plural: 'models',
+		initialize: function()
+		{
+			this.ran_init = true;
+		}
+	};
+
+	var Model, instance, another, hookTest, hookid;
+
+	before(function()
+	{
+		Model = polyclay.Model.buildClass(modelDefinition);
+		polyclay.persist(Model);
+	});
+
+	it('can be configured for database access', function(done)
+	{
+		var options =
+		{
+			host:     'localhost',
+			port:     28015,
+			database: 'test',
+		};
+
+		Model.setStorage(options, Adapter);
+		Model.adapter.must.exist();
+		Model.adapter.constructor.must.equal(Model);
+		Model.adapter.tablename.must.equal(Model.prototype.plural);
+		Model.adapter.on('ready', function()
+		{
+			Model.adapter.must.have.property('connection');
+			done();
+		});
+	});
+
+	it('provision creates the database & table, if necessary', function(done)
+	{
+		Model.provision(function(err)
+		{
+			// check for existence of db
+			// check for existence of table
+			demand(err).not.exist();
+			done();
+		});
+	});
+
+	it('provision can be called more than once safely', function(done)
+	{
+		Model.provision(function(err)
+		{
+			demand(err).not.exist();
+			done();
+		});
+	});
+
+	it('throws when asked to save a document without a key', function()
+	{
+		var noID = function()
+		{
+			var obj = new Model();
+			obj.name = 'idless';
+			obj.save(function(err, reply)
+			{
+			});
+		};
+
+		noID.must.throw(Error);
+	});
+
+	it('can save a document in the db', function(done)
+	{
+		instance = new Model();
+		instance.update(
+		{
+			key: '1',
+			name: 'test',
+			created: Date.now(),
+			foozles: ['three', 'two', 'one'],
+			snozzers: { field: 'value' },
+			is_valid: true,
+			count: 3,
+			required_prop: 'requirement met',
+			computed: 17
+		});
+
+		instance.save(function(err, reply)
+		{
+			demand(err).not.exist();
+			reply.must.exist();
+			done();
+		});
+	});
+
+	it('can retrieve the saved document', function(done)
+	{
+		Model.get(instance.key, function(err, retrieved)
+		{
+			demand(err).not.exist();
+			retrieved.must.exist();
+			retrieved.must.be.an.object();
+			retrieved.key.must.equal(instance.key);
+			retrieved.name.must.equal(instance.name);
+			retrieved.created.getTime().must.equal(instance.created.getTime());
+			retrieved.is_valid.must.equal(instance.is_valid);
+			retrieved.count.must.equal(instance.count);
+			retrieved.computed.must.equal(instance.computed);
+			done();
+		});
+	});
+
+	it('can update the document', function(done)
+	{
+		instance.name = "New name";
+		instance.isDirty().must.be.true();
+		instance.save(function(err, response)
+		{
+			demand(err).not.exist();
+			response.must.be.a.string();
+			response.must.equal('OK');
+			instance.isDirty().must.equal(false);
+			done();
+		});
+	});
+
+
+	it('can fetch in batches', function(done)
+	{
+		var ids = [ instance.key ];
+		var obj = new Model();
+		obj.name = 'two';
+		obj.key = '2';
+		obj.save(function(err, response)
+		{
+			ids.push(obj.key);
+
+			Model.get(ids, function(err, itemlist)
+			{
+				demand(err).not.exist();
+				itemlist.must.be.an.array();
+				itemlist.length.must.equal(2);
+				done();
+			});
+		});
+	});
+
+	it('get() can handle an id or an array of ids', function(done)
+	{
+		var ids = [ '1', '2' ];
+		Model.adapter.get(ids, function(err, itemlist)
+		{
+			demand(err).not.exist();
+			itemlist.must.be.an.array();
+			itemlist.length.must.equal(2);
+			done();
+		});
+	});
+
+	it('can fetch all', function(done)
+	{
+		Model.all(function(err, itemlist)
+		{
+			demand(err).not.exist();
+			itemlist.must.be.an.array();
+			itemlist.length.must.be.at(2);
+			done();
+		});
+	});
+
+	it('constructMany() retuns an empty list when given empty input', function(done)
+	{
+		Model.constructMany([], function(err, results)
+		{
+			demand(err).not.exist();
+			results.must.be.an.array();
+			results.length.must.equal(0);
+			done();
+		});
+	});
+
+	it('merge() updates properties then saves the object', function(done)
+	{
+		Model.get('2', function(err, item)
+		{
+			demand(err).not.exist();
+
+			item.merge({ is_valid: true, count: 1023 }, function(err, response)
+			{
+				demand(err).not.exist();
+				Model.get(item.key, function(err, stored)
+				{
+					demand(err).not.exist();
+					stored.count.must.equal(1023);
+					stored.is_valid.must.equal(true);
+					stored.name.must.equal(item.name);
+					done();
+				});
+			});
+		});
+	});
+
+	it('can add an attachment type', function()
+	{
+		Model.defineAttachment('frogs', 'text/plain');
+		Model.defineAttachment('avatar', 'image/png');
+
+		instance.set_frogs.must.be.a.function();
+		instance.fetch_frogs.must.be.a.function();
+		var property = Object.getOwnPropertyDescriptor(Model.prototype, 'frogs');
+		property.get.must.be.a.function();
+		property.set.must.be.a.function();
+	});
+
+	it('can save attachments', function(done)
+	{
+		instance.avatar = attachmentdata;
+		instance.frogs = 'This is bunch of frogs.';
+		instance.isDirty().must.equal.true;
+		instance.save(function(err, response)
+		{
+			demand(err).not.exist();
+			instance.isDirty().must.equal.false;
+			done();
+		});
+	});
+
+	it('can retrieve attachments', function(done)
+	{
+		Model.get(instance.key, function(err, retrieved)
+		{
+			retrieved.fetch_frogs(function(err, frogs)
+			{
+				demand(err).not.exist();
+				frogs.must.be.a.string();
+				frogs.must.equal('This is bunch of frogs.');
+				retrieved.fetch_avatar(function(err, imagedata)
+				{
+					demand(err).not.exist();
+					imagedata.must.be.instanceof(Buffer);
+					imagedata.length.must.equal(attachmentdata.length);
+					done();
+				});
+			});
+		});
+	});
+
+	it('can update an attachment', function(done)
+	{
+		instance.frogs = 'Poison frogs are awesome.';
+		instance.save(function(err, response)
+		{
+			demand(err).not.exist();
+			Model.get(instance.key, function(err, retrieved)
+			{
+				demand(err).not.exist();
+				retrieved.fetch_frogs(function(err, frogs)
+				{
+					demand(err).not.exist();
+					frogs.must.equal(instance.frogs);
+					retrieved.fetch_avatar(function(err, imagedata)
+					{
+						demand(err).not.exist();
+						imagedata.length.must.equal(attachmentdata.length);
+						done();
+					});
+				});
+			});
+		});
+	});
+
+	it('can store an attachment directly', function(done)
+	{
+		instance.frogs = 'Poison frogs are awesome, but I think sand frogs are adorable.';
+		instance.saveAttachment('frogs', function(err, response)
+		{
+			demand(err).not.exist();
+			Model.get(instance.key, function(err, retrieved)
+			{
+				demand(err).not.exist();
+				retrieved.fetch_frogs(function(err, frogs)
+				{
+					demand(err).not.exist();
+					frogs.must.equal(instance.frogs);
+					done();
+				});
+			});
+		});
+	});
+
+	it('saveAttachment() clears the dirty bit', function(done)
+	{
+		instance.frogs = 'This is bunch of frogs.';
+		instance.isDirty().must.equal(true);
+		instance.saveAttachment('frogs', function(err, response)
+		{
+			demand(err).not.exist();
+			instance.isDirty().must.equal(false);
+			done();
+		});
+	});
+
+	it('can remove an attachment', function(done)
+	{
+		instance.removeAttachment('frogs', function(err, deleted)
+		{
+			demand(err).not.exist();
+			deleted.must.be.true();
+			done();
+		});
+	});
+
+
+	it('caches an attachment after it is fetched', function(done)
+	{
+		instance.avatar = attachmentdata;
+		instance.save(function(err, response)
+		{
+			demand(err).not.exist();
+			instance.isDirty().must.be.false();
+			instance.fetch_avatar(function(err, imagedata)
+			{
+				demand(err).not.exist();
+				var cached = instance.__attachments['avatar'].body;
+				cached.must.exist();
+				(cached instanceof Buffer).must.equal(true);
+				polyclay.dataLength(cached).must.equal(polyclay.dataLength(attachmentdata));
+				done();
+			});
+		});
+	});
+
+	it('can fetch an attachment directly', function(done)
+	{
+		Model.adapter.attachment('1', 'avatar', function(err, body)
+		{
+			demand(err).not.exist();
+			body.must.be.instanceof(Buffer);
+			polyclay.dataLength(body).must.equal(polyclay.dataLength(attachmentdata));
+			done();
+		});
+	});
+
+	it('removes an attachment when its data is set to null', function(done)
+	{
+		instance.avatar = null;
+		instance.save(function(err, response)
+		{
+			demand(err).not.exist();
+			Model.get(instance.key, function(err, retrieved)
+			{
+				demand(err).not.exist();
+				retrieved.fetch_avatar(function(err, imagedata)
+				{
+					demand(err).not.exist();
+					demand(imagedata).not.exist();
+					done();
+				});
+			});
+		});
+	});
+
+	it('can remove a document from the db', function(done)
+	{
+		instance.destroy(function(err, deleted)
+		{
+			demand(err).not.exist();
+			deleted.must.exist();
+			instance.destroyed.must.be.true();
+			done();
+		});
+	});
+
+	it('can remove documents in batches', function(done)
+	{
+		var obj2 = new Model();
+		obj2.key = '4';
+		obj2.name = 'two';
+		obj2.save(function(err, response)
+		{
+			Model.get('2', function(err, obj)
+			{
+				demand(err).not.exist();
+				obj.must.be.an.object();
+
+				var itemlist = [obj, obj2.key];
+				Model.destroyMany(itemlist, function(err, response)
+				{
+					demand(err).not.exist();
+					// TODO examine response more carefully
+					done();
+				});
+			});
+		});
+	});
+
+	it('destroyMany() does nothing when given empty input', function(done)
+	{
+		Model.destroyMany(null, function(err)
+		{
+			demand(err).not.exist();
+			done();
+		});
+	});
+
+	it('destroy responds with an error when passed an object without an id', function(done)
+	{
+		var obj = new Model();
+		obj.destroy(function(err, destroyed)
+		{
+			err.must.be.an.object();
+			err.message.must.equal('cannot destroy object without an id');
+			done();
+		});
+	});
+
+	it('destroy responds with an error when passed an object that has already been destroyed', function(done)
+	{
+		var obj = new Model();
+		obj.key = 'foozle';
+		obj.destroyed = true;
+		obj.destroy(function(err, destroyed)
+		{
+			err.must.be.an.object();
+			err.message.must.equal('object already destroyed');
+			done();
+		});
+	});
+
+	it('removes attachments when removing an object', function(done)
+	{
+		var obj = new Model();
+		obj.key = 'cats';
+		obj.frogs = 'Cats do not eat frogs.';
+		obj.name = 'all about cats';
+
+		obj.save(function(err, reply)
+		{
+			demand(err).not.exist();
+			reply.must.equal('OK');
+
+			obj.destroy(function(err, destroyed)
+			{
+				demand(err).not.exist();
+				done();
+			});
+		});
+	});
+
+	after(function(done)
+	{
+		Rethink.dbDrop('test')
+			.run(Model.adapter.connection, function(err)
+		{
+			done();
+		});
+	});
+});
